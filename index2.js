53b6cf68706899293d082fbe2cabe067215485d7
CRNE <47609964+CRNE@users.noreply.github.com>
2019-02-13 21:14:12 +0000

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+
+
+var data = [14,	7,	18,	7, 4, 16, 2, 9, 6, 3, 10, 4]; 
+
+var labels = ["Offshore wind",	"Solar",	"Biomass",	"Energy from waste",	"Other renewable",	"Oil and gas", "Thermal power",	"LNG",	"Gas to power", "Geothermal",	"Battery storage", "Other"];
+
+
+// End Defining data
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Headlights with Barclays', // Name the series
+            data: data, // Specify the data values array
+            backgroundColor: [ // Specify custom colors
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [ // Add custom color borders
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1 // Specify bar border width
+        }]
+    },
+    options: {
+      responsive: true, // Instruct chart js to respond nicely.
+      maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height
+    }
+});
